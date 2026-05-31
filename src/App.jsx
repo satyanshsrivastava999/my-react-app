f4b642550a726888cf22c69092a78f82b3176e8b
SATYANSH SRIVASTAVA <satyanshsrivastava253@gmail.com>
2026-05-31 23:31:25 +0530

    updated app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1>Welcome to my Web Pages of ABES</h1>
+      <h2>Hello</h2>
+      <p>Hello Rohit Sir</p>
+
+      <img
+        src="https://images.pexels.com/photos/36578421/pexels-photo-36578421.jpeg"
+        width="400"
+      />
+    </>
+  );
+}
+
+export default App;
