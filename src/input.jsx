c119e727e67828da0e6c469b7dc35a742e88eb5a
SATYANSH SRIVASTAVA <satyanshsrivastava253@gmail.com>
2026-06-17 23:26:49 +0530

    new file

---
+++ src/input.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <h2>{name}</h2>
+    </>
+  );
+}
