9b175b4c6ca64ca2b44a3a44f442f344b5633861
SATYANSH SRIVASTAVA <satyanshsrivastava253@gmail.com>
2026-06-17 23:28:20 +0530

    new file

---
+++ src/renderring list.jsx
@@ -0,0 +1,11 @@
+function App() {
+  const fruits = ["Apple", "Banana", "Mango"];
+
+  return (
+    <ul>
+      {fruits.map((fruit, index) => (
+        <li key={index}>{fruit}</li>
+      ))}
+    </ul>
+  );
+}
